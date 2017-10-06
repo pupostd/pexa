54ad8b7a723030d7402f8f148c3d86c76e100b1c
pupostd <pupo@tutanota.com>
2017-10-06 14:45:45 -0300

    Working with command params.

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
 	fmt.Printf("Let's begin!\n")
+
+	var name string
+
+	var run = flag.String("r", "none", "Name of the function you want to run.")
+	flag.StringVar(&name, "n", "none", "Some name to say hello!")
+
+	flag.Parse()
+
+	fmt.Println("Running: ", *run)
+
+	switch *run {
+	case "hello":
+		hello(name)
+	default:
+		fmt.Println("Hi ..")
+
+	}
+
+}
+func hello(name string) {
+	fmt.Printf("Hello %s!\n", name)
 }
